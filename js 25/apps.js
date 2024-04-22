0568f30e94f85f0c465515cb86735bcac2a3987d
zohaibsyl <159802070+zohaibsyl@users.noreply.github.com>
2024-04-22 03:32:30 -0700

    add all files

---
+++ js 25/apps.js
@@ -0,0 +1,14 @@
+// function sum(a,b,c){
+//     var result = a +b + c;
+//     return result
+// }
+// var  res = sum (10 ,20 ,30);
+// console.log(res);
+
+// function bioData(fullname ,lastname, age, city){
+//     var completeData = "my name is " + fullname + " " + lastname + "my age is" +
+//      " " + age + "i live in"+" "   + city;
+//     return completeData
+// }
+
+// console.log(bioData("zohaib"+ " "+"ahmed"+  " " +20 + " " + "karachi"));
